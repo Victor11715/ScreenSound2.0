9832aead4eced30b8765d45486f4af245adad3bc
Victor Hugo Notari <victorhugonotari@gmail.com>
2024-04-22 15:19:00 -0300

    Criação das propriedades da classe Musica

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -3,17 +3,7 @@
     public string nome;
     public string artista;
     public int duracao;
-    private bool disponivel;
-
-    public void EscreveDisponivel(bool value)
-    {
-        disponivel = value;
-    }
-
-    public bool LeDisponivel()
-    {
-        return disponivel;
-    }
+    public bool Disponivel { get; set; }
 
     public void ExibirFicaTecnica()
     {
