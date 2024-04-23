3c997b918d14fd7881c8993adbb588d2032da17f
Victor Hugo Notari <victorhugonotari@gmail.com>
2024-04-23 17:07:15 -0300

    Exibindo as músicas de um álbum.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,10 +2,14 @@
 albumDoQueen.Nome = "A Night at the Opera";
 
 Musica musica1 = new Musica();
-musica1.Nome = "Love of My Live";
+musica1.Nome = "Love of My Life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
